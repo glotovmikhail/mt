96eea706e3cc567b691e1891410a212884e162bb
Mikhail <glotov.mikhail@mail.ru>
2018-09-23 00:36:28 +0300

    lab 2 Pascal logic formulas

---
+++ src/lab2/LexicalAnalyzer.java
@@ -0,0 +1,111 @@
+package lab2;
+
+enum Token {
+    NOT, AND, OR, XOR,
+    LPAREN, RPAREN, END, BOOL
+}
+
+class LexicalAnalyzer {
+    private String data;
+    private int pointer;
+    private long dataSize;
+    private String currBool;
+    private Token currentToken = Token.END;
+
+    LexicalAnalyzer(String s) throws LexicalException {
+        this.data = s;
+        this.pointer = 0;
+        this.dataSize = s.length();
+        nextToken();
+    }
+
+    private void trim() {
+        char c;
+        while (pointer < dataSize && ((c = data.charAt(pointer)) == ' ' || c == '\r' || c == '\n' || c == '\t')) {
+            pointer++;
+        }
+    }
+
+    void nextToken() throws LexicalException {
+        trim();
+        Token answer;
+        if (pointer == dataSize) {
+            currentToken = Token.END;
+            return;
+        }
+        switch (data.charAt(pointer)) {
+            case '(':
+                if (eat("(")) {
+                    currentToken = Token.LPAREN;
+                    return;
+                }
+            case ')':
+                if (eat(")")) {
+                    currentToken = Token.RPAREN;
+                    return;
+                }
+            case 'o':
+                if (eat("or")) {
+                    currentToken = Token.OR;
+                    return;
+                }
+            case 'a':
+                if (eat("and")) {
+                    currentToken = Token.AND;
+                    return;
+                }
+            case 'n':
+                if (eat("not")) {
+                    currentToken = Token.NOT;
+                    return;
+                }
+            case 'x':
+                if (eat("xor")) {
+                    currentToken = Token.XOR;
+                    return;
+                }
+            default:
+                if (eatVal()) {
+                    currentToken = Token.BOOL;
+                    return;
+                }
+        }
+        throw new LexicalException("");
+    }
+
+    private Boolean eat(String s) {
+        long sSize = s.length();
+        if (dataSize < pointer + sSize) return false;
+        for (int i = 0; i < sSize; i++) {
+            if (s.charAt(i) != data.charAt(pointer + i)) return false;
+        }
+        pointer += sSize;
+        return true;
+    }
+
+    private Boolean eatVal() {
+        char c;
+        Boolean flag = false;
+        currBool="";
+        while ((dataSize > pointer) && ((c = data.charAt(pointer)) >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')) {
+            currBool += data.charAt(pointer);//
+            pointer++;
+            flag = true;
+        }
+        return flag;
+    }
+
+    Token currToken() {
+        return currentToken;
+    }
+
+    String getCurrBool() {
+        return currBool;
+    }
+}
+
+class LexicalException extends Exception {
+    LexicalException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/lab2/Main.java
@@ -0,0 +1,62 @@
+package lab2;
+
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.layout.HierarchicalLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Main {
+    private static SingleGraph graph;
+    private static PrintWriter p;
+
+    public static void main(String[] args) throws FileNotFoundException, LexicalException, ParseException {
+        String tests[] ={"a or b or c",
+                "a xor b or c",
+                "a or b and c",
+                "a or (b and c)",
+                "(((a or (((b)) and c))))",
+                "  b    or (  a    and   (    not                geez)        and \na and c \t)      ",
+                "not not a and not (not b or c and (not not e)))"
+        };
+
+
+        graph = new SingleGraph("");
+        graph.setNullAttributesAreErrors(true);
+        graph.addAttribute("ui.stylesheet", "node {size: 5px; fill-color: black;} " +
+                                                    "node.marked {size: 7px; fill-color: red, blue;}");
+        graph.addAttribute("ui.quality");
+        graph.addAttribute("ui.antialias");
+
+        Viewer viewer = graph.display();
+        HierarchicalLayout layout = new HierarchicalLayout();
+        viewer.enableAutoLayout(layout);
+
+
+        Node root = Parser.parse(tests[3]);
+        p = new PrintWriter(new File("output.txt"));
+        org.graphstream.graph.Node rootNode = print(root, "");
+        p.close();
+
+        rootNode.setAttribute("ui.class", "marked");
+        rootNode.setAttribute("ui.color", 0.5);
+
+
+        viewer.getDefaultView().getCamera().resetView();
+        graph.addAttribute("ui.screenshot", "./screenshot.png");
+    }
+
+    private static org.graphstream.graph.Node print(Node parserNode, String tab) {
+        org.graphstream.graph.Node node = graph.addNode(parserNode.id.toString());
+        node.setAttribute("ui.label", parserNode.interp);
+
+        p.println(tab + parserNode.interp + " : ");
+        for (Node i : parserNode.children) {
+            print(i, tab + "\t");
+            graph.addEdge(parserNode.id + "|" + i.id, parserNode.id.toString(), i.id.toString());
+        }
+        return node;
+    }
+}
